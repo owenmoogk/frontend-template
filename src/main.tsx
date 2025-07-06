af915f359b68f4f2072567b0b8002b985b476bcc
Owen Moogk <owenmoogk@gmail.com>
2025-07-06 13:42:04 -0400

    Change to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import '@mantine/core/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import { mantineCssVariableResolver } from './cssVariableResolver';
